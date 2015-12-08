2e2c661d5e642591eee93f52c87f375d08b03a31
gareth4192 <gareth-123@live.com>
2015-12-08 15:29:20 +0000

    player 2 hp visible

---
+++ spec/features/player_points_spec.rb
@@ -0,0 +1,9 @@
+feature 'Hit points' do
+  scenario 'player 2s hit points' do
+      visit('/')
+      fill_in :player_1_name, with: 'Dave'
+      fill_in :player_2_name, with: 'Mittens'
+      click_button 'Submit'
+      expect(page).to have_content 'Mittens HP:'
+  end
+end
